a3584eaa1eec2a9d74d038805d118d710c0a1e20
mbrad <mbrad@DESKTOP-ROCK425>
2022-05-31 01:02:19 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    string translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e) {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber)) {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        } else {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+
+    async void OnCall(object sender, System.EventArgs e) {
+        if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No")) {
+            try {
+                PhoneDialer.Open(translatedNumber);
+            } catch (ArgumentNullException) {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            } catch (FeatureNotSupportedException) {
+                await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+            } catch (Exception) {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+}
+
